f9a0212e81b1a95133400d494f65f1fd1fd16fd9
João Monteiro <jbmjbmjbm>
2019-04-20 20:46:23 -0300

    atualização do projeto

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 spl_autoload_register(function($class_naame){
 
-	$filename = $class_naame.".php";
+	$filename = "class". DIRECTORY_SEPARATOR. $class_naame.".php";
 
 	if (file_exists($filename)){
 		require_once($filename);
