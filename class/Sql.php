d5d7817f06102bbe8e593d461d7a220ca48f215c
João Monteiro <jbmjbmjbm>
2019-04-28 16:44:06 -0300

    DAO insert

---
+++ class/Sql.php
@@ -0,0 +1,52 @@
+<?php 
+
+class Sql extends PDO{
+
+	private $conn;
+
+	public function __construct(){
+
+		$this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
+		//("sqlsrv:Database=dbphp7;server=localhost\SQLEXPRESS;ConnectionPooling=0", "sa", "root");
+		//("mysql:host=localhost;dbname=dbphp7", "root", "");
+
+	}
+
+	private function setParams($estatement, $parameters = array()){
+
+		foreach ($parameters as $key => $value) {
+			$this->setParam($estatement, $key, $value);
+		}
+	}
+
+	private function setParam($estatement, $key, $value){
+
+		$estatement->bindParam($key, $value);
+
+	}
+
+	public function query($rawQuwey, $params = array()){
+
+		$stmt = $this->conn->prepare($rawQuwey);
+
+		$this->setParams($stmt, $params);
+
+		$stmt->execute();
+
+		return $stmt;
+		
+	}
+
+	public function select($rawQuery, $params = array()):array
+	{
+
+		$stmt = $this->query($rawQuery, $params);
+
+		return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+	}
+
+}
+ //merdsssss :)
+
+ ?>
---
+++ index.php
@@ -0,0 +1,56 @@
+<?php 
+
+require_once("config.php");
+
+//$sql = new Sql();
+
+//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+
+//echo json_encode($usuarios);
+
+
+//---------------------------------------------
+
+//carrega um usuário
+
+//$root  = new Usuario();
+
+//$root->loadbyID(3);
+
+//echo $root;
+
+//---------------------------------
+
+//carrega uma lista de usuários.
+
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//---------------------------------
+
+//carrega uma lista de usuario buscando pelo id
+//$search = Usuario::search("te");
+
+//echo json_encode($search);
+
+
+//----------------------------------------
+
+// carrega um usuario usando 0 login e senha
+
+//$usuario = new Usuario();
+//$usuario->login("Joao", "helena@123");
+
+//echo $usuario;
+
+//----------------------
+
+// insert de um aluno novo
+
+$aluno = new Usuario("aluno", "@alun0");
+
+$aluno->insert();
+
+echo $aluno;
+
+?>
